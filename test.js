590f66a28f1dde1049f4a94e44e736ff960a32b8
Stan Cosmin <94742249+StanCosmin28@users.noreply.github.com>
2023-10-04 22:31:53 +0300

    new update for biolink

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-
+gsap.fromTo(
+  "#poza",
+  { y: -40 },
+  { y: 0, duration: 0.6, yoyo: true, repeat: -1 }
+);
+gsap.fromTo(
+  "#scale",
+  { rotate: "-1deg" },
+  {
+    rotate: "1deg",
+    duration: 0.2,
+    yoyo: true,
+    repeat: -1,
+  }
+);
